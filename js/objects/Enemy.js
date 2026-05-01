20bb9b88723437541452add90b5dbcd312749cef
JuanPabloSch <juanpablosch@gmail.com>
2026-05-01 17:38:51 -0300

    tackle

--- js/objects/Enemy.js
+++ js/objects/Enemy.js
@@ -9,9 +9,28 @@ export class Enemy {
         this.sprite.setTint(0xff4444);
         this.sprite.setDepth(2);
         this.sprite.setCollideWorldBounds(true);
+        this.stunned = false;
+        this.stunTimer = 0;
     }
 
     update() {
+
+        if (this.stunned) {
+
+    this.stunTimer -= this.scene.game.loop.delta;
+
+    this.sprite.body.setVelocity(0);
+    this.sprite.setTint(0xffff00);
+    this.sprite.stop();
+    this.sprite.setFrame(0);
+
+    if (this.stunTimer <= 0) {
+        this.stunned = false;
+        this.sprite.setTint(0xff4444);
+    }
+
+    return;
+}
 
     let speed = 150;
 
@@ -24,9 +43,17 @@ export class Enemy {
 
     // si tiene pelota va al arco izquierdo
     if (this.ball.owner === 'enemy') {
-        targetX = this.scene.offsetX;
-        targetY = this.scene.centerY;
+
+    targetX = this.scene.offsetX + 30;
+    targetY = this.scene.centerY;
+
+    // si llegó cerca del arco, patea
+    if (this.sprite.x < this.scene.offsetX + 220) {
+
+        this.ball.shoot(-420, 0);
+        this.ball.owner = 'none';
     }
+}
 
     const dx = targetX - this.sprite.x;
     const dy = targetY - this.sprite.y;
@@ -34,12 +61,30 @@ export class Enemy {
     const dist = Math.sqrt(dx * dx + dy * dy);
 
     if (dist > 5) {
+
         this.sprite.body.setVelocity(
             (dx / dist) * speed,
             (dy / dist) * speed
         );
+
+        // mirar lado correcto
+        if (dx < 0) this.sprite.flipX = true;
+        else this.sprite.flipX = false;
+
+        // animar
+        if (!this.sprite.anims.isPlaying) {
+            this.sprite.play('run');
+        }
+
     } else {
+
         this.sprite.body.setVelocity(0);
+        this.sprite.stop();
+        this.sprite.setFrame(0);
     }
-}   
+}
+stun(ms = 1000) {
+    this.stunned = true;
+    this.stunTimer = ms;
+}
 }
